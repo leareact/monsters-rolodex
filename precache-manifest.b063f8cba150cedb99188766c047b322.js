4954b48ff3b7e384f1bd2f110712e70bbd25f1d7
leareact <anja.me97991@gmail.com>
2020-01-10 01:36:36 +0200

    Updates

---
+++ precache-manifest.b063f8cba150cedb99188766c047b322.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "405449afef6a492683e8cf3436f8562e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1dd8248bc8a4d076a494",
+    "url": "/monsters-rolodex/static/css/main.d6da11d1.chunk.css"
+  },
+  {
+    "revision": "55ee059b2988e93ca287",
+    "url": "/monsters-rolodex/static/js/2.279358c6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.279358c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1dd8248bc8a4d076a494",
+    "url": "/monsters-rolodex/static/js/main.e1ea6ba1.chunk.js"
+  },
+  {
+    "revision": "d90ac9b9402da2dda087",
+    "url": "/monsters-rolodex/static/js/runtime-main.f8e4036a.js"
+  }
+]);
